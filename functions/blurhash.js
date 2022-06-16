712d21b4aa27ba420ecce296d3db47e9a9c5c88e
colbyfayock <fayock@gmail.com>
2022-06-16 14:47:12 +0300

    Step 4: parsing multi-part form data

--- functions/blurhash.js
+++ functions/blurhash.js
@@ -1,4 +1,8 @@
+const parser = require('lambda-multipart-parser');
+
 exports.handler = async (event, context) => {
+  const formData = await parser.parse(event);
+
   return {
     statusCode: 200,
     body: JSON.stringify({ status: 'Ok' })
